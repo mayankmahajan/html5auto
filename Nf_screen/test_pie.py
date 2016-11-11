491184918719397643ec251342f3d77d33fe721a
vkagg05 <vk.agg05@gmail.com>
2016-11-11 10:48:21 +0530

    11/10/2016

---
+++ Nf_screen/test_pie.py
@@ -0,0 +1,51 @@
+import unittest
+from Utils.logger import *
+from selenium import webdriver
+
+from Utils.utility import *
+from classes.DriverHelpers.DriverHelper import DriverHelper
+from Utils.Constants import *
+from Utils.SetUp import *
+
+# Getting Setup Details and Launching the application
+setup = SetUp()
+
+# Logging into the appliction
+login(setup, "admin", "Admin@123")
+
+# Launch Site Screen
+launchPage(setup,"site_Screen")
+
+
+# Get the Instance of the screen
+screenInstance = SitePageClass(setup.d)
+
+# Get the handles of the screenx
+siteScreenHandle = getHandle(setup,"site_Screen")
+
+
+# Get the default selection
+#defSelection = screenInstance.btv.getSelection(siteScreenHandle)
+
+
+# Validating the result
+#checkEqualAssert(str(1),str(defSelection['selIndex']),"","")
+
+# Set the bar Table view to the 2 index
+screenInstance.btv.setSelection(2,siteScreenHandle)
+drilltoScreen(setup.d,setup.dH,Constants.NETWORKFUNCTIONS)
+
+# screeinstancenf = SitePageClass(setup.d)
+# nfScreenHandle = getHandle(setup,Constants.NETWORKFUNCTIONS)
+
+# Get the Instance of the screen
+screenInstance1 = PieLegendComponentClass(SetUp.D)
+
+# Get the handles of the screenx
+nfScreenHandle = getHandle(setup,Constants.NETWORKFUNCTIONS)
+
+screenInstance1.setSelectionIndex()
+
+
+setup.d.close()
+#checkEqualAssert("True",result,"","","drillToNF")
