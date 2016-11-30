96e62085791d1959d2c5cd4e5eb71b6675be7972
nehaiiitm14 <neha.gupta@neha-gupta.local>
2016-11-30 17:09:02 +0530

    Interface screen

---
+++ Interface_screen/test_inteface.py
@@ -0,0 +1,31 @@
+from Utils.SetUp import *
+from Utils.utility import *
+from classes.Pages.InterfacePageClass import *
+
+#######################################################################
+# Getting Setup Details
+setup = SetUp()
+#######################################################################
+
+
+#######################################################################
+#get time range and measures from config file
+#timeIteration = len(setup.cM.getNodeElements("quicklinks","quicklink"))
+#quicklinks = setup.cM.getNodeElements("quicklinks","quicklink").keys()
+t=0
+
+#measureIteration = len(setup.cM.getNodeElements("measures","measure"))
+#measures = setup.cM.getNodeElements("measures","measure").keys()
+#######################################################################
+
+#######################################################################
+# Logging into the appliction and launch site screen
+login_status=login(setup, "admin", "Admin@123")
+#checkEqualAssert(True,login_status,"","","Login to NRMCA UI")
+launch_status=launchPage(setup,Constants.INTERFACES)
+checkEqualAssert(True,launch_status,"","","launch site screen")
+sleep(5)
+screenInstance = InterfacePageClass(setup.d)
+#setMeasure(setup,set_measure,"site_Screen")
+interfaceScreenHandle = getHandle(setup,Constants.INTERFACES)
+print screenInstance.table.getIterfaceTableData(interfaceScreenHandle)
