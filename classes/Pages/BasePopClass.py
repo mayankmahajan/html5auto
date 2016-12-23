b759de3cabdf1a5482bf3cb44dad5dc060afe5f5
Mayank Mahajan <Mayank.Mahajan@guavus.com>
2016-12-23 09:44:55 +0530

    updating switcher for reportsmodule

--- classes/Pages/BasePopClass.py
+++ classes/Pages/BasePopClass.py
@@ -3,14 +3,16 @@ from classes.Components.MeasureComponentClass import *
 from classes.Components.SummaryBarComponentClass import *
 from classes.Components.QuicklinkTimeRangeComponentClass import *
 from classes.Components.SearchComponentClass import *
+from classes.Components.SwitcherComponentClass import *
 
 class BasePopClass():
     def __init__(self,driver):
         pass
 
 
-        # self.measure = MeasureComponentClass()
-        # self.summarybar = SummaryBarComponentClass()
-        # self.quiklinkTimeRange = QuicklinkTimeRangeComponentClass()
-        # self.searchComp = SearchComponentClass()
-        # self.cm = ContextMenuComponentClass()
+        self.measure = MeasureComponentClass()
+        self.summarybar = SummaryBarComponentClass()
+        self.quiklinkTimeRange = QuicklinkTimeRangeComponentClass()
+        self.searchComp = SearchComponentClass()
+        self.cm = ContextMenuComponentClass()
+        self.switcher = SwitcherComponentClass()
