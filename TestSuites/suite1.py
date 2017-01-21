cc152b1a5e57c91d798747bb1479c288c30c1099
Mayank Mahajan <Mayank.Mahajan@guavus.com>
2017-01-21 11:27:26 +0530

    updating script to incorporate all suites

--- TestSuites/suite1.py
+++ TestSuites/suite1.py
@@ -1,6 +1,6 @@
 import glob
 
-test_file_strings = glob.glob('../Tests/test_*.py')
+test_file_strings = glob.glob('../suite_*/test_*.py')
 module_strings = [str.split('/')[1] + "." + str.split('/')[2].split('.')[0] for str in test_file_strings]
 [__import__(str) for str in module_strings]
 
