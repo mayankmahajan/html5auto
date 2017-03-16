b7df0dcb855e32cf4c8da1baaeada1915dacfc0b
CS-BLDE-1033\Administrator <mayank.mahajan@guavus.com>
2017-03-16 16:12:01 +0530

    Updated Code for quickLink

--- suitemural_network/AccessTechnologyScript.py
+++ suitemural_network/AccessTechnologyScript.py
@@ -6,6 +6,7 @@ from classes.Components.WorkflowStartComponent import *
 from classes.Components.TimeRangeComponentClass import *
 from classes.Pages.MuralScreens.AccessTechnologyClass import *
 from suitemural_network.test_accesstechnology_general import *
+from classes.Components.TimeRangeComponentClass import *
 import sys
 
 screen="Access Technology"
@@ -34,11 +35,16 @@ try:
 
         #accesstechnologyScreenInstance.timeBar.getLabel(getHandle(setup, MuralConstants.ATSCREEN, "ktrs"))
         #accesstechnologyScreenInstance.timeBar.getSelectedQuickLink(getHandle(setup, MuralConstants.ATSCREEN, "ktrs"))
+
         accesstechnologyScreenInstance.timeBar.setQuickLink(qs[e]['locatorText'],getHandle(setup, MuralConstants.ATSCREEN, "ktrs"))
         selectedQuicklink = networkScreenInstance.timeBar.getSelectedQuickLink(getHandle(setup, MuralConstants.ATSCREEN, "ktrs"))
 
+        t = TimeRangeComponentClass().get_Label(e)
+        t1 = accesstechnologyScreenInstance.timeBar.getLabel(getHandle(setup, MuralConstants.ATSCREEN, "ktrs"))
+        checkEqualAssert(t[1], t1, selectedQuicklink, "", "verify quicklink label")
+
         screenName = accesstechnologyScreenInstance.cm.getScreenName(getHandle(setup, MuralConstants.ATSCREEN, "exploreBar"))
-        checkEqualAssert(screen,str(screenName),e,"","Verify Screen Name")
+        checkEqualAssert(screen,str(screenName),selectedQuicklink,"","Verify Screen Name")
         breadCrumbLabel = accesstechnologyScreenInstance.cm.getRHSBreadCrumbLabel(getHandle(setup, MuralConstants.ATSCREEN, "exploreBar"))
 
         measures = setup.cM.getNodeElements("networkdimeas", "measure")
@@ -46,35 +52,35 @@ try:
         for k, measure in measures.iteritems():
             if not measure.has_key("summaryCard"):
                 measureSelected = accesstechnologyScreenInstance.picker.domultipleSelectionWithName(getHandle(setup,MuralConstants.NWSCREEN,"measureChangeSection"),measure['locatorText'],0,"measureChangeSection","measure")
-                checkEqualAssert(measure['locatorText'],measureSelected,e,measure['locatorText'],"Verify Selected measure")
+                checkEqualAssert(measure['locatorText'],measureSelected,selectedQuicklink,measure['locatorText'],"Verify Selected measure")
 
                 if measure.has_key("options") and 'direction' in measure['options']:
                     isDirectionsPresent = accesstechnologyScreenInstance.switcher.getMeasureChangeSelectedSwitcher(getHandle(setup, MuralConstants.NWSCREEN, "measureChangeSection"))
-                    checkEqualAssert(list,type(isDirectionsPresent), e, measure['locatorText'],"Verify presence of Directions for Measure = " + measureSelected)
+                    checkEqualAssert(list,type(isDirectionsPresent), selectedQuicklink, measure['locatorText'],"Verify presence of Directions for Measure = " + measureSelected)
 
                     for d in range(0,3):
                         if accesstechnologyScreenInstance.switcher.measureChangeSwitcher(d,getHandle(setup,MuralConstants.ATSCREEN,"measureChangeSection")):
                             selectedSwitcher = accesstechnologyScreenInstance.switcher.getMeasureChangeSelectedSwitcher(getHandle(setup,MuralConstants.ATSCREEN,"measureChangeSection"))
-                            checkEqualAssert([d],selectedSwitcher,e,measure['locatorText'],"Verify Selected Measure Direction")
-                            checkAllComponentRelatedToPie(setup,accesstechnologyScreenInstance,e,measure,0,False)
+                            checkEqualAssert([d],selectedSwitcher,selectedQuicklink,measure['locatorText'],"Verify Selected Measure Direction")
+                            checkAllComponentRelatedToPie(setup,accesstechnologyScreenInstance,selectedQuicklink,measure,0,False)
                             #checkAllComponentRelatedToPie(setup, instance, measureSelected, index, flag)
 
                             p = accesstechnologyScreenInstance.pielegend.getData(getHandle(setup, MuralConstants.ATSCREEN, "pielegend"))
                             for i in range(len(p['legendText'])):
                                 accesstechnologyScreenInstance.pielegend.setSelection(setup.dH, [i],getHandle(setup, MuralConstants.ATSCREEN, "pielegend"), True)
                                 # sleep(8)
-                                checkAllComponentRelatedToPie(setup, accesstechnologyScreenInstance, e,measure,i,True)
+                                checkAllComponentRelatedToPie(setup, accesstechnologyScreenInstance, selectedQuicklink,measure,i,True)
 
                 else:
                     isDirectionsPresent = accesstechnologyScreenInstance.switcher.getMeasureChangeSelectedSwitcher(getHandle(setup, MuralConstants.NWSCREEN, "measureChangeSection"))
-                    checkEqualAssert(False, isDirectionsPresent, e, measure['locatorText'],"Verify presence of Directions for Measure = " + measureSelected)
+                    checkEqualAssert(False, isDirectionsPresent, selectedQuicklink, measure['locatorText'],"Verify presence of Directions for Measure = " + measureSelected)
                     d = None
-                    checkAllComponentRelatedToPie(setup, accesstechnologyScreenInstance, e,measure, 0,False)
+                    checkAllComponentRelatedToPie(setup, accesstechnologyScreenInstance, selectedQuicklink,measure, 0,False)
 
                     p = accesstechnologyScreenInstance.pielegend.getData(getHandle(setup, MuralConstants.ATSCREEN, "pielegend"))
                     for i in range(len(p['legendText'])):
                         accesstechnologyScreenInstance.pielegend.setSelection(setup.dH, [i],getHandle(setup, MuralConstants.ATSCREEN,"pielegend"), True)
-                        checkAllComponentRelatedToPie(setup, accesstechnologyScreenInstance, e,measure, i, True)
+                        checkAllComponentRelatedToPie(setup, accesstechnologyScreenInstance, selectedQuicklink,measure, i, True)
 
     toolTipPieAndPieLegend(setup, accesstechnologyScreenInstance)
     accesstechnologyScreenInstance.cm.activate(getHandle(setup, MuralConstants.ATSCREEN, "exploreBar"), child="export")
